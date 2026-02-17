684abc54928a9f6993e118e1e6eb4fdedd23c8a0
Matteo Raggi <matteo.raggi04@gmail.com>
2026-02-17 16:30:25 +0100

    style: fix PWA

--- src/App.tsx
+++ src/App.tsx
@@ -27,7 +27,7 @@ const App: React.FC = () => {
       <InstallPWA />
       <Router>
         {/* Main App Container - Fixed to viewport */}
-        <div className="fixed inset-0 bg-gray-50 flex flex-col">
+        <div className="fixed inset-0 bg-gray-50 flex flex-col pt-[env(safe-area-inset-top)] pb-0">
 
           {/* Scrollable Content Area */}
           <div className="flex-1 overflow-y-auto overflow-x-hidden relative w-full">
